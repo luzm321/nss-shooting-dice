7b3a7ebb80690dd56276c7ccf04b29257b232fbf
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-09-03 13:45:58 -0500

    completed OneHigherPlayer class and instantiated new class to create another Player object

--- OneHigherPlayer.cs
+++ OneHigherPlayer.cs
@@ -1,10 +1,34 @@
+using System;
+
 namespace ShootingDice
 {
     // TODO: Complete this class
 
     // Override the Play method to make a Player who always roles one higher than the other player
-    public class OneHigherPlayer
+    public class OneHigherPlayer : Player
     {
+        public override void Play(Player other)
+        {
+            // Call roll for "this" object and for the "other" object
+            int otherRoll = other.Roll();
+            int myRoll = otherRoll + 1; // ensures that this type of player will always roll one higher than the other player
 
+            Console.WriteLine($"{Name} rolls a {myRoll}");
+            Console.WriteLine($"{other.Name} rolls a {otherRoll}");
+            Console.WriteLine($"{Name} rolls one higher than the other player! :O");
+            if (myRoll > otherRoll)
+            {
+                Console.WriteLine($"{Name} Wins!");
+            }
+            else if (myRoll < otherRoll)
+            {
+                Console.WriteLine($"{other.Name} Wins!");
+            }
+            else
+            {
+                // if the rolls are equal it's a tie
+                Console.WriteLine("It's a tie");
+            }
+        }
     }
 }
